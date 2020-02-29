70226b8ba94a370abe50ddf56110a46f8ff0100f
iyasmzn <iyasmzn07@gmail.com>
2020-02-29 23:08:26 +0700

    update night

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,12 +1,20 @@
-<div class="sidebar p-3 text-josefin">
-	<div>
-		<h3>Category</h3>
-		<ul>
-			<li>asd</li>
-			<li>asd</li>
-			<li>asd</li>
-			<li>asd</li>
-			<li>asd</li>
-		</ul>
+<div class="sidebar text-josefin">
+	<div class="sidebar-content-1">
+		<h3 class="my-4">Category</h3>
+			<a class="p-1 pl-2 category-list"><i class="fas fa-circle text-danger"></i> NEWS <span class="badge badge-danger">99+</span></a>
+			<a class="p-1 pl-2 category-list"><i class="fas fa-circle text-success"></i> FILM ACTORS <span class="badge badge-info">2</span></a>
+			<a class="p-1 pl-2 category-list"><i class="fas fa-circle text-primary"></i> VEHICLE <span class="badge badge-info">9</span></a>
+			<a class="p-1 pl-2 category-list"><i class="fas fa-circle text-warning"></i> ANIMATION FILM <span class="badge badge-info">6</span></a>
+			<a class="p-1 pl-2 category-list"><i class="fas fa-circle text-dark"></i> SPORT <span class="badge badge-warning">52</span></a>
+			<a class="p-1 pl-2 category-list"><i class="fas fa-circle text-tomato"></i> EVENT <span class="badge badge-success">1</span></a>
+	</div>
+	<div class="sidebar-content-2">
+		<h3 class="my-4">most commented</h3>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="most-commented-img-1"></div>
+			</div>
+			<div class="col-sm-8">asd</div>
+		</div>
 	</div>
 </div>
