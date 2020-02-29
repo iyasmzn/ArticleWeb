7ab226fb78a8c41d7696289c344a339175b7facf
iyasmzn <iyasmzn07@gmail.com>
2020-02-29 21:59:48 +0700

    add partials

---
+++ partials/sidebar.php
@@ -0,0 +1,12 @@
+<div class="sidebar p-3 text-josefin">
+	<div>
+		<h3>Category</h3>
+		<ul>
+			<li>asd</li>
+			<li>asd</li>
+			<li>asd</li>
+			<li>asd</li>
+			<li>asd</li>
+		</ul>
+	</div>
+</div>
