c2b56672135b87762fdb13ef435084b404cbc068
iyasmzn <iyasmzn07@gmail.com>
2020-02-29 21:59:39 +0700

    update hot article

--- index.php
+++ index.php
@@ -61,8 +61,8 @@
 
 		</div>
 	</div>
-	<div class="container-fluid my-3 mb-5 px-5">
-			<h1 class="text-center text-josefin text-tomato mt-5 py-3" style="font-weight: bold">HEAD ARTICLES!</h1>
+	<div class="head-newss container-fluid py-3 pb-5 px-5 bg-tomato">
+			<h1 class="text-left text-anton text-light mt-5 py-3" style="font-weight: bold">HOT ARTICLES!</h1>
 			<div class="owl-carousel owl-theme hot-news-carousel p-0 m-0">
 			    <div class="item hot-news-carousel-1">
 			    	<div class="hot-news-carousel-img-1 ">
@@ -72,7 +72,7 @@
 							<div class="row p-3" style="text-decoration: underline;">
 								<div class="col-sm-6">
 
-									<span>NEWS</span>
+									<span class="badge badge-danger">NEWS</span>
 
 								</div>
 								<div class="col-sm-6 text-right">
@@ -90,7 +90,7 @@
 
 							<p>Its not impossible now, for human to stay in the mars. And its ill take 10 person for first try to the sun. The cost are very cheap because...</p>
 
-							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READMORE</a>
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
 						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
@@ -101,7 +101,7 @@
 							<div class="row p-3" style="text-decoration: underline;">
 								<div class="col-sm-6">
 
-									<span>ARTICLE</span>
+									<span class="badge badge-primary">VEHICLE</span>
 
 								</div>
 								<div class="col-sm-6 text-right">
@@ -119,58 +119,191 @@
 
 							<p>Mercedes Benz now have a unbelievable car! The speed is very fast, it can reach 350mph! And the interior is...</p>
 
-							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READMORE</a>
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
 						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-3 "></div>
+			    	<div class="hot-news-carousel-img-3 ">
+							<div class="blacker-news"></div>
+			    	</div>
+			    	<div class="hot-news-tag text-josefin text-light">
+							<div class="row p-3" style="text-decoration: underline;">
+								<div class="col-sm-6">
+
+									<span class="badge badge-warning">ANIMATION FILM</span>
+
+								</div>
+								<div class="col-sm-6 text-right">
+									
+									<span>22 Comment</span>
+
+								</div>
+							</div>
+						</div>
+						<div class="hot-news-title text-josefin p-5 text-light">
+
+							<p class="">FEBRUARY 29, 2020</p>
+
+							<h4 class="text-anton">Best Nominated Oscar Animation Movies</h4>
+
+							<p>Woody and the gang go on a road trip with new toy Forky, who’s convinced he’s trash and doesn’t yet understand his role in the world. Along the...</p>
+
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
+						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-4 "></div>
+			    	<div class="hot-news-carousel-img-4 ">
+							<div class="blacker-news"></div>
+			    	</div>
+			    	<div class="hot-news-tag text-josefin text-light">
+							<div class="row p-3" style="text-decoration: underline;">
+								<div class="col-sm-6">
+
+									<span class="badge badge-success">FILM ACTORS</span>
+
+								</div>
+								<div class="col-sm-6 text-right">
+									
+									<span>22 Comment</span>
+
+								</div>
+							</div>
+						</div>
+						<div class="hot-news-title text-josefin p-5 text-light">
+
+							<p class="">FEBRUARY 29, 2020</p>
+
+							<h4 class="text-anton">Joaquin Phoenix Get Winner in Oscar Nominated Actor</h4>
+
+							<p>In Gotham’s fractured society, a young man longs to be a stand-up comic, but finds the joke is always on him. Caught in...</p>
+
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
+						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-5 "></div>
+			    	<div class="hot-news-carousel-img-1 ">
+							<div class="blacker-news"></div>
+			    	</div>
+			    	<div class="hot-news-tag text-josefin text-light">
+							<div class="row p-3" style="text-decoration: underline;">
+								<div class="col-sm-6">
+
+									<span class="badge badge-danger">NEWS</span>
+
+								</div>
+								<div class="col-sm-6 text-right">
+									
+									<span>1 Comment</span>
+
+								</div>
+							</div>
+						</div>
+						<div class="hot-news-title text-josefin p-5 text-light">
+
+							<p class="">FEBRUARY 29, 2020</p>
+
+							<h4 class="text-anton">ASTRONOUT WILL GO TO THE MARS IN FEBRUARY 31, 2021!</h4>
+
+							<p>Its not impossible now, for human to stay in the mars. And its ill take 10 person for first try to the sun. The cost are very cheap because...</p>
+
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
+						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-6 "></div>
+			    	<div class="hot-news-carousel-img-2 ">
+							<div class="blacker-news"></div>
+			    	</div>
+			    	<div class="hot-news-tag text-josefin text-light">
+							<div class="row p-3" style="text-decoration: underline;">
+								<div class="col-sm-6">
+
+									<span class="badge badge-primary">VEHICLE</span>
+
+								</div>
+								<div class="col-sm-6 text-right">
+									
+									<span>12 Comment</span>
+
+								</div>
+							</div>
+						</div>
+						<div class="hot-news-title text-josefin p-5 text-light">
+
+							<p class="">FEBRUARY 29, 2020</p>
+
+							<h4 class="text-anton">New Unbelievable Mercy Sport Car</h4>
+
+							<p>Mercedes Benz now have a unbelievable car! The speed is very fast, it can reach 350mph! And the interior is...</p>
+
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
+						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-7 "></div>
+			    	<div class="hot-news-carousel-img-3 ">
+							<div class="blacker-news"></div>
+			    	</div>
+			    	<div class="hot-news-tag text-josefin text-light">
+							<div class="row p-3" style="text-decoration: underline;">
+								<div class="col-sm-6">
+
+									<span class="badge badge-warning">ANIMATION FILM</span>
+
+								</div>
+								<div class="col-sm-6 text-right">
+									
+									<span>22 Comment</span>
+
+								</div>
+							</div>
+						</div>
+						<div class="hot-news-title text-josefin p-5 text-light">
+
+							<p class="">FEBRUARY 29, 2020</p>
+
+							<h4 class="text-anton">Best Nominated Oscar Animation Movies</h4>
+
+							<p>Woody and the gang go on a road trip with new toy Forky, who’s convinced he’s trash and doesn’t yet understand his role in the world. Along the...</p>
+
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
+						</div>
 			    </div>
 			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-8 "></div>
-			    </div>
-			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-9 "></div>
-			    </div>
-			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-10 "></div>
-			    </div>
-			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-11 "></div>
-			    </div>
-			    <div class="item hot-news-carousel-1">
-			    	<div class="hot-news-carousel-img-12 "></div>
+			    	<div class="hot-news-carousel-img-4 ">
+							<div class="blacker-news"></div>
+			    	</div>
+			    	<div class="hot-news-tag text-josefin text-light">
+							<div class="row p-3" style="text-decoration: underline;">
+								<div class="col-sm-6">
+
+									<span class="badge badge-success">FILM ACTORS</span>
+
+								</div>
+								<div class="col-sm-6 text-right">
+									
+									<span>22 Comment</span>
+
+								</div>
+							</div>
+						</div>
+						<div class="hot-news-title text-josefin p-5 text-light">
+
+							<p class="">FEBRUARY 29, 2020</p>
+
+							<h4 class="text-anton">Joaquin Phoenix Get Winner in Oscar Nominated Actor</h4>
+
+							<p>In Gotham’s fractured society, a young man longs to be a stand-up comic, but finds the joke is always on him. Caught in...</p>
+
+							<a href="#" class="btn btn-lg btn-outline-light" style="border-radius: 0px;">READ MORE</a>
+						</div>
 			    </div>
 			</div>
 	</div>
 	<div class="w-100 p-3 pt-5" style="background-color: tomato">
-		<div class="container">
+		<div class="container-fluid">
 			<div class="row" style="">
 				<div class="news-dashboard col-md-8">as</div>
 				<div class="col-md-4">
-					<div class="sidebar p-3 text-josefin">
-						<div>
-							<h3>Category</h3>
-							<ul>
-								<li>asd</li>
-								<li>asd</li>
-								<li>asd</li>
-								<li>asd</li>
-								<li>asd</li>
-							</ul>
-						</div>
-					</div>
+					<?php include './partials/sidebar.php'; ?>
 				</div>
 			</div>
 		</div>
